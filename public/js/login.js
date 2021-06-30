5d998f48859145b7335a36938261b62e22084213
Travis Brown <bergannation@gmail.com>
2021-06-30 13:30:58 -0500

    adding logout/login logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,27 @@
-const loginFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
+async function loginFormHandler(event) {
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
-  const email = document.querySelector('#email-login').value.trim();
+  const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
-    // TODO: Add a comment describing the functionality of this expression
+  if (username && password) {
     const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
-      alert('Failed to log in');
+      alert(response.statusText);
     }
   }
-};
+}
 
 document
-  .querySelector('.login-form')
+  .querySelector('#login-form')
   .addEventListener('submit', loginFormHandler);
