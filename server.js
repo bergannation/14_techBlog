a64dad4b6a831515d1eaeeca58a68da104891e7b
Travis Brown <bergannation@gmail.com>
2021-07-06 16:47:22 -0500

    adding views to fix rendering

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const exphbs = require('express-handlebars');
 
 const helpers = require('./utils/helpers');
 
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,13 +26,11 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers })
-
 app.engine('handlebars', hbs.engine);
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
